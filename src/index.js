1bcf7517160c7aff0612a8caf495eeb0441680aa
rjarrin <jarrin.ricardo@gmail.com>
2024-04-29 16:39:09 -0400

    Implemented buildTree() function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+/* eslint-disable no-console */
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/extensions */
-import Tree from "./tree.js";
+import Tree from './tree.js';
+
+function testTree() {
+    console.log('Testing BST...');
+    // Create a BST from an array of numbers  TODO: USE RANDOM NUMBERS FOR FINAL TESTING
+    const array = [10, 20, 30, 40, 50, 1, 100, 65, 64, 67, 80];
+    console.log(`Creating BST from array: ${array}`);
+    const bst = new Tree(array);
+    console.log(bst);
+    console.log('Reached end of testTree()');
+}
+
+testTree();
--- src/tree.js
+++ src/tree.js
@@ -1,11 +1,30 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/extensions */
-import Node from "./node.js";
+import Node from './node.js';
 
 class Tree {
     constructor(array) {
         this.root = this.buildTree(array);
+    }
+
+    // Build the BST from the parameter array
+    buildTree(array) {
+        // Remove dulpicates from the array by converting it to a Set
+        const unqiueArray = new Set(array);
+        // Convert the Set back to an array and sort it in ascending order for balancing
+        const sortedArray = [...unqiueArray].sort((a, b) => a - b);
+        // Base case: if the sorted array is empty (i.e., empty tree), return null
+        if (sortedArray.length === 0) return null;
+        // Determine tree root by identifying array median
+        const middleIndex = Math.floor(sortedArray.length / 2);
+        // Create the root
+        const root = new Node(sortedArray[middleIndex]);
+        // Recursively build the left subtree using the lesser elements
+        root.leftChild = this.buildTree(sortedArray.slice(0, middleIndex));
+        // Recursively build the right subtree using the greater elements
+        root.rightChild = this.buildTree(sortedArray.slice(middleIndex + 1));
+        return root;
     }
 }
 
